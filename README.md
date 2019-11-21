41cf1c0f1ba2722b0fc35b073d9595fd1f11c59b
dduits <devdduits@gmail.com>
2019-11-21 15:38:08 +0100

    Added a readme and improved code comments

---
+++ README.md
@@ -0,0 +1,29 @@
+# Utils
+
+Collection of utility scripts and tools that do various things.
+
+## Getting Started
+
+Clone the project and get started with the script you want to use.
+
+```
+https://github.com/dduits/utils.git
+```
+
+### Prerequisites
+
+The prerequisites and dependencies differ per script / tool.
+
+#### internship-datetime-utils
+
+* Python 3
+
+## Authors
+
+* **Devananda Duits** - *Initial work* - [dduits](https://github.com/dduits)
+
+See also the list of [contributors](https://github.com/dduits/utils/graphs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- internship-datetime-utils.py
+++ internship-datetime-utils.py
@@ -8,31 +8,31 @@ start_date = list(map(int, start_date.split("-")))
 # Convert to a date.
 start_date = date(start_date[2], start_date[1], start_date[0])
 
-# Get the amount of hours for the internship.
+# Get the duration of the internship in hours.
 hour_amount = input("Amount of hour required for the intership: \n> ").strip()
 hour_amount = int(hour_amount)
 
 # Get the amount of work hours a week.
-week_hours = input("Amount of hours a week: \n> ").strip()
+week_hours = input("Amount of hours per week: \n> ").strip()
 week_hours = int(week_hours)
 
 # Calculate the amount of weeks.
 weeks = hour_amount / week_hours
-print(weeks)
 
 # Get extra weeks.
 extra_weeks = input("Extra amount of weeks (leave empty for none): \n> ")
 
-# Add extra weeks if required.
+# Add extra weeks if necessary.
 if extra_weeks:
     extra_weeks = int(extra_weeks)
     weeks = weeks + extra_weeks
 
-# Calculate the end date and print the result.
+# Calculate the end date.
 end_date = start_date + timedelta(weeks=weeks)
 
 # Set the day to monday if the calculated end date is in the weekend.
 if end_date.weekday() > 4:
+    # Add two days if saturday and add only one if sunday.
     monday_addition = 2 if end_date.weekday() == 5 else 1
     end_date = end_date + timedelta(days=monday_addition)
 
