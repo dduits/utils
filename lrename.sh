f8702622c975ef5a6bf4e54e1658fddcef2265b3
dduits <devdduits@gmail.com>
2019-12-31 22:20:07 +0100

    Changed documentation formatting for lrename

--- lrename.sh
+++ lrename.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#title     		: lrename.sh
-#description    : Renames a file or folder to be lowercase and have hyphen for spaces.
-#author			: dduits (https://github.com/dduits)
-#license		: MIT, see the LICENSE file for more information.
-#version        : 1.0
-#usage		 	: sh lrename.sh ~/Documents/File\ With\ Spaces.txt
+# title: lrename.sh
+# description: Renames a file or folder to be lowercase and have hyphen for spaces.
+# author: dduits (https://github.com/dduits)
+# license: MIT, see the LICENSE file for more information.
+# version: 1.0
+# usage: sh lrename.sh ~/Documents/File\ With\ Spaces.txt
 
 file=$1
 parent_dir="$(dirname "$file")"
