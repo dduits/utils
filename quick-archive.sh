56995b06e5077baf434ae47f700c068c87606906
dduits <devdduits@gmail.com>
2019-12-31 22:01:52 +0100

    Updated quick-archive script

--- quick-archive.sh
+++ quick-archive.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
+#title     		: quick-archive.sh
+#description   	: Provides a quicker way to archiving folders. 
+#author			: dduits (https://github.com/dduits)
+#license		: MIT, see the LICENSE file for more information.
+#version        : 1.0
+#usage		 	: ./quick-archive.sh ~/Documents/school ~/Documents
 
-folder_path=$1
-destination_path=$2
+folder=$1
+destination=$2
 
-folder_basename=$(basename "$folder_path")
-folder_parent_path="$(readlink -f "$(basename "$(dirname "$folder_path")")")"
+# Make sure the folder was provided and exist.
+if [ -z "$folder" ] || [ ! -d "$folder" ]; then
+    echo "ERR: Folder was invalid."
+    exit 1
+fi
 
-archive_file_name="$folder_basename.tar.bz2"
-archive_path="$(readlink -f "$2")"
+# Make sure the destination folder was provided and exist.
+if [ -z "$destination" ] || [ ! -d "$destination" ]; then
+    echo "ERR: Destination folder was invalid."
+    exit 1
+fi
 
-tar cjf "$archive_path"/"$archive_file_name" -C "$folder_parent_path" "$folder_basename"
+# Get the name of the folder.
+folder_basename=$(basename "$folder")
+# Get the parent directory.
+parent_dir=$(dirname "$folder")
+
+# Archive name e.g. Documents.tar.bz2.
+archive_filename="$folder_basename.tar.bz2"
+
+# Create the archive.
+tar cjf "$destination"/"$archive_filename" -C "$parent_dir" "$folder_basename"
